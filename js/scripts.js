91590ba0f6ded213f68abb7f62c874fe2b30a6a2
Daniel Maples <bables85@gmail.com>
2023-02-25 13:04:02 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(function() {
+    
     $(".carousel").carousel( { interval: 2000 } );
     $("#carouselButton").click(function(){
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
@@ -10,5 +11,13 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
+    });
+
+    $("#reserveButton").on("click", function(){
+        $("#reserveModal").modal('toggle');
+    });
+
+    $("#loginButton").on("click", function(){
+        $("#loginModal").modal('toggle');
     });
 });
